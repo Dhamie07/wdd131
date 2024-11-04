cd30d75e911401e91b8a99745ee87191b7c6b8f5
Dhamie07 <okpewodamilola@gmail.com>
2024-11-04 08:35:20 +0100

    home page

---
+++ scripts/getdates.js
@@ -0,0 +1,10 @@
+// Set the current year
+const currentYear = new Date().getFullYear();
+document.getElementById("currentyear").textContent = currentYear;
+
+// Set your name
+document.getElementById("yourname").textContent = "🌻Okpewo Damilola James🌻";
+
+// Set the last modified date
+const lastModifiedDate = document.lastModified;
+document.getElementById("lastModified").textContent = `Last modified: ${lastModifiedDate}`;
